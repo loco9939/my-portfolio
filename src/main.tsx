fdc2f0f59f13726a02bc2b5d9d8f5b1889cfeb07
김이주 <yiju@gim-ijuui-MacBookPro.local>
2024-01-07 00:37:17 +0900

    feat(main): 라우터 등록 및 css 우선순위 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
+import { StyledEngineProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/_reset.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import ErrorPage from "./page/errorPage/index.tsx";
+import Login from "./page/login/index.tsx";
+import SignUp from "./page/signup/index.tsx";
+import "./styles/_reset.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+      {
+        path: "/signin",
+        element: <Login />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    {/* NOTE: CSS-in-JS 방식에서 custom-style이 덮어씌워지는 문제 해결 */}
+    <StyledEngineProvider injectFirst>
+      <RouterProvider router={router} />
+    </StyledEngineProvider>
   </React.StrictMode>
 );
