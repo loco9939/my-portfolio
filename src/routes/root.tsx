7b1a8e2621cdfc80d5aedfed89086ed5de95afe6
loco9939 <kls9939@naver.com>
2024-01-26 06:28:32 +0900

    fix(라우터): 로그인 상태에 따라 분기 처리 추가

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,28 +1,27 @@
 import { Box } from "@mui/material";
 import "./root.css";
-import { useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import App from "@/App";
 import { useLogin } from "./root.hooks";
 import { useEffect } from "react";
 
 function Root() {
   const navigate = useNavigate();
+  const location = useLocation();
   const isLoggedIn = useLogin();
 
   useEffect(() => {
     if (!isLoggedIn) {
       navigate("/signin");
+    } else if (location.pathname === "/") {
+      navigate("/dashboard");
     }
-  });
+  }, [isLoggedIn, location.pathname, navigate]);
 
   return (
     <>
-      <Box
-        component={"header"}
-        className="header"
-        onClick={() => navigate("/")}
-      >
-        My Portfolio
+      <Box component={"header"} className="header">
+        <Link to={"/"}>My Portfolio</Link>
       </Box>
       <Box component={"main"}>
         <App />
