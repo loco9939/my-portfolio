5df8dda8b522261f79a6fd8c110098672cd8fa29
김이주 <150204547+WATA-Yiju@users.noreply.github.com>
2024-03-07 22:33:06 +0900

    feat(dashboard): 월별 보기 기능 추가

--- src/page/dashboard/index.tsx
+++ src/page/dashboard/index.tsx
@@ -1,36 +1,26 @@
+import MyPiechart from "@/components/chart/MyPiechart";
+import MyStackedBarchart from "@/components/chart/MyStackedBarchart";
 import { convertPieChartData } from "@/utils/convertPieChartData";
+import { convertStackedBarChartData } from "@/utils/convertStackedBarChartData";
 import { getLastUpdateDate, getUserFinancial } from "@/utils/getUser";
 import { Box, Button, Typography } from "@mui/material";
-import { PieChart, pieArcLabelClasses } from "@mui/x-charts";
 import { useState } from "react";
 import styles from "./dashboard.module.css";
 
 type Tab = "monthly" | "proportion";
 
 function Dashboard() {
-  const financialData = getUserFinancial();
+  const monthlyAssets = getUserFinancial();
   const lastUpdate = getLastUpdateDate();
 
   const [tab, setTab] = useState<Tab>("proportion");
 
   const onClickTab = (tab: Tab) => setTab(tab);
 
-  const totalFinancialData = Object.values(financialData ?? {}).reduce(
-    (acc, cur) => {
-      const { cashWon, saving, stock, realEstate, debt } = cur;
-      return {
-        cashWon: acc.cashWon + cashWon,
-        saving: acc.saving + saving,
-        stock: acc.stock + stock,
-        realEstate: acc.realEstate + realEstate,
-        debt: acc.debt + debt,
-      };
-    },
-    { cashWon: 0, saving: 0, stock: 0, realEstate: 0, debt: 0 }
-  );
-
-  const pieChartData = convertPieChartData(totalFinancialData);
+  const pieChartData = convertPieChartData(monthlyAssets ?? {});
   const assetsSum = pieChartData.reduce((acc, data) => acc + data.value, 0);
+
+  const stackedBarChartData = convertStackedBarChartData(monthlyAssets ?? {});
 
   return (
     <Box component={"section"} className={styles.wrapper}>
@@ -38,7 +28,7 @@ function Dashboard() {
         나의 자산 통계
       </Typography>
 
-      <Box component={"div"} className={styles.buttonWrapper}>
+      <Box component={"section"} className={styles.buttonWrapper}>
         <Button
           variant={tab === "proportion" ? "contained" : "outlined"}
           onClick={() => onClickTab("proportion")}
@@ -53,35 +43,22 @@ function Dashboard() {
         </Button>
       </Box>
 
-      <Box component={"div"} className={styles.chartWrapper}>
-        <Box>
-          <PieChart
-            series={[
-              {
-                data: pieChartData,
-                innerRadius: 5,
-                paddingAngle: 2,
-                cornerRadius: 5,
-                valueFormatter: (value) =>
-                  `${value.value.toLocaleString("ko-KR")} 원`,
-                arcLabel: (item) =>
-                  `${((item.value / assetsSum) * 100).toFixed(2)} %`,
-              },
-            ]}
-            sx={{
-              [`& .${pieArcLabelClasses.root}`]: {
-                fill: "white",
-                fontWeight: "bold",
-                fontSize: "18px",
-              },
-            }}
-            width={600}
-            height={400}
-          />
-          <Typography margin={4} variant="body2">
-            {lastUpdate} 기준
-          </Typography>
-        </Box>
+      <Box component={"section"} className={styles.chartWrapper}>
+        {tab === "proportion" ? (
+          <Box component={"div"}>
+            <MyPiechart data={pieChartData} assetsSum={assetsSum} />
+            <Typography margin={4} variant="body2">
+              {lastUpdate} 기준
+            </Typography>
+          </Box>
+        ) : (
+          <Box component={"div"}>
+            <MyStackedBarchart data={stackedBarChartData} />
+            <Typography margin={4} variant="body2">
+              {lastUpdate} 기준
+            </Typography>
+          </Box>
+        )}
       </Box>
     </Box>
   );
