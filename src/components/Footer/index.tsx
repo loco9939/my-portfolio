c1b43c4f800f446faf3f1c34e68e46120d04c16a
WATA-Yiju <yiju.dev@watanow.com>
2024-04-05 16:24:17 +0900

    feat(Footer): 푸터 내용 추가

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,9 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <Box component={"footer"} className={styles.footer}>
-      My Portfolio footer
+      <p>My Portfolio - Copyright for loco9939 @2024 </p>
+      <p>Email: kls9939@naver.com</p>
+      <p>github: https://github.com/loco9939</p>
     </Box>
   );
 };
